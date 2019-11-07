b9978854c303ed2d960ca16b6e5c2de50cafe53e
Henry Chung <chungthehao@gmail.com>
2019-11-07 21:52:19 +0700

    Card List Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React /*, { Component }*/ from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends React.Component {
@@ -22,11 +25,13 @@ class App extends React.Component {
     // Tat ca moi thu nhin giong HTML deu la JSX (div, src, p, button, onClick,...)
     return (
       <div className='App'>
-        {// Tra ve 1 mang cac JSX, React tu hieu la render nhung cai trong mang do.
-        // Key de React biet duoc thang nao thay doi thi chi update thang do thoi, ko re-render lai toan bo ca list
-        this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList thamSo={1 + 3} name='Henry Chung'>
+          {// Tra ve 1 mang cac JSX, React tu hieu la render nhung cai trong mang do.
+          // Key de React biet duoc thang nao thay doi thi chi update thang do thoi, ko re-render lai toan bo ca list
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
