890574fee4765ee9b86a1aa8a39607742a5ac417
Henry Chung <chungthehao@gmail.com>
2019-11-08 23:20:05 +0700

    Updates

---
+++ precache-manifest.ee3901534bfa5afa2733734370988845.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3dcbf616865702da792a228b543016",
+    "url": "/React-Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "073a4a819b97ff772c92",
+    "url": "/React-Monsters-Rolodex/static/css/main.91d5d518.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/React-Monsters-Rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "073a4a819b97ff772c92",
+    "url": "/React-Monsters-Rolodex/static/js/main.fb5eb0aa.chunk.js"
+  },
+  {
+    "revision": "3b2c89d46c369f46ad32",
+    "url": "/React-Monsters-Rolodex/static/js/runtime-main.747238d1.js"
+  }
+]);
